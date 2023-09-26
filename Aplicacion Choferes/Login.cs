571d3884796f9f88a3efcf54224287c6657d2324
damian <suffodamian02@gmail.com>
2023-09-26 16:37:11 -0300

    estructura basica

--- Aplicacion Choferes/Login.cs
+++ Aplicacion Choferes/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Aplicacion_Choferes
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
--- Aplicacion Choferes/Program.cs
+++ Aplicacion Choferes/Program.cs
@@ -16,7 +16,7 @@ namespace Aplicacion_Choferes
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
